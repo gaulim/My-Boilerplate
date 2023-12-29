dbf2b9aeafb2d249339a75d10a948748086f90db
가울림 <4607540+gaulim@users.noreply.github.com>
2023-12-29 12:24:04 +0900

    feat: 스프링부트 Kotlin 프로젝트를 만듬
    - SpringBoot 3.2.0
    - Kotlin 1.9.20
    - Gradle 8.5 (Gradle-Groovy)
    - JVM 21

---
+++ spring-boot-kotlin/src/main/kotlin/io/ssaragi/kotlin/spring/boot/SpringBootKotlinApplication.kt
@@ -0,0 +1,11 @@
+package io.ssaragi.kotlin.spring.boot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootKotlinApplication>(*args)
+}
