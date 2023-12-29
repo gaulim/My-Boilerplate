0d2018ef41c02d27be0d1f67351cb06bc7f169f2
가울림 <4607540+gaulim@users.noreply.github.com>
2023-12-29 12:17:38 +0900

    feat: 스프링부트 Java 프로젝트를 만듬
    - SpringBoot 3.2.0
    - Gradle 8.5 (Gradle-Groovy)
    - JVM 21

---
+++ spring-boot-java/src/test/java/io/ssaragi/java/spring/boot/SpringBootJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ssaragi.java.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
