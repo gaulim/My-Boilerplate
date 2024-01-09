ee75dd2dc5cd17d6710e75f70c135a12e1b28579
가울림 <4607540+gaulim@users.noreply.github.com>
2024-01-09 23:29:35 +0900

    feat: 스프링부트 Java 에 SpringSecurity 적용

---
+++ spring-boot-java/src/main/java/io/ssaragi/java/spring/boot/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package io.ssaragi.java.spring.boot.config;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request -> request
+                    .requestMatchers("/*/echo/*")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated()
+                )
+                .sessionManagement(session -> session
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .exceptionHandling(handler -> handler
+                    .authenticationEntryPoint(
+                        (request, response, authException) ->
+                            response.sendError(HttpServletResponse.SC_NOT_FOUND)
+                    )
+                )
+                .build();
+
+    }
+}
